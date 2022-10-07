0655da164d068edcd94c0cce761ee2aa49de236f
Tyler McQueen <tylerryanmcqueen@gmail.com>
2022-10-07 16:54:55 -0400

    added user account login/registration
    added user account login/registration
    changed login-page UI

--- Test_Project/test_project/lib/screens/authenticate/sign_in.dart
+++ Test_Project/test_project/lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,6 @@
-import 'dart:ui';
+// ignore_for_file: sort_child_properties_last
 
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:test_project/services/auth.dart';
 
@@ -13,6 +14,8 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
 
   final AuthService _auth = AuthService();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -24,21 +27,70 @@ class _SignInState extends State<SignIn> {
         title: Text('Sign Into Magic Recipe'),
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: ElevatedButton(
-          child: Text('Sign In Anon'),
-          onPressed: () async {
-            dynamic result = await _auth.signInAnon();
-            if (result == null){
-              print('error signing in');
-            } else {
-              print('signed in');
-              print(result.uid);
-            }
-          }
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10),
+              child:TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter A Email',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child:TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter A Password',
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
-
+        
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(height: 70.0),
+        ),
+        floatingActionButton: Stack(
+          children: [
+            Positioned(
+              left: 100,
+              bottom: 40,
+              child: FloatingActionButton.extended(
+                icon: Icon(Icons.login),
+                onPressed: () async {await _auth.signOut();
+                  dynamic result = await _auth.registerEmailAccount(emailController.text, passwordController.text);
+                }, 
+              label: Text('Register'),
+              ),
+            ),
+            Positioned(
+              right: 100,
+              bottom: 40,
+              child: FloatingActionButton.extended(
+                icon: Icon(Icons.login),
+                onPressed: () async {
+                    dynamic result = await _auth.signinEmailAccount(emailController.text, passwordController.text);
+                    if (result == null){
+                      print('error signing in');
+                    } else {
+                      print('signed in');
+                      print(result.uid);
+                    }
+                }, 
+              label: Text('LOGIN'),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        
     );
   }
 }

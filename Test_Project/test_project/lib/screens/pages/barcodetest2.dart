4555ffdb4896f4f394544af7c5f56a82832603dc
Tyler <tylerryanmcqueen@gmail.com>
2022-11-13 16:25:32 -0500

    barcode results page

--- Test_Project/test_project/lib/screens/pages/barcodetest2.dart
+++ Test_Project/test_project/lib/screens/pages/barcodetest2.dart
@@ -20,7 +20,34 @@ class BarcodeTest4State extends State<BarcodeTest4> {
               debugPrint('Failed to scan Barcode');
             } else {
               final String code = barcode.rawValue!;
+
               debugPrint('Barcode found! $code');
+              showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0))),
+                        content: Builder(
+                          builder: (context) {
+                            // Get available height and width of the build area of this widget. Make a choice depending on the size.
+                            var height = MediaQuery.of(context).size.height;
+                            var width = MediaQuery.of(context).size.width;
+
+                            return Container(
+                              height: height,
+                              width: width,
+                              child: Scaffold(
+                                appBar:
+                                    AppBar(title: const Text('Mobile Scanner')),
+                                body: (Column(
+                                  children: [Text('$code')],
+                                )),
+                              ),
+                            );
+                          },
+                        ),
+                      ));
             }
           }),
     );
